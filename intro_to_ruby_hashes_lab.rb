9fc7de9b025c68fd0faa7591efcfc07db73b8159
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-22 22:45:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  #return a hash with one key called :railroads that points to an empty hash
+  #{:railroads =>{}}
+  {railroads: {}}
 
 end
 
@@ -9,6 +12,16 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  # {:railroads => {
+   #  :pieces => "4",
+    # :rent_in_dollars => {},
+    # :names => {}
+   #}}
+ {railroads: {
+   pieces: 4,
+   rent_in_dollars: {},
+   names: {}
+ }}
 
 end
 
